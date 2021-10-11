37ac7f5e4eb7d9a5a48499078084d0f13b70c5a3
Elene khubua <elenekhubua@yahoo.com>
2021-10-11 23:23:52 +0400

    add changes

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ document.getElementById('send').addEventListener('click', function(){
 })
 
 
+let navigation=document.getElementById('navigation');
+let burgerIcon=document.getElementById('burgerIcon');
 
+burgerIcon.addEventListener('click', function() {
+    navigation.classList.toggle('active');
+});
 
 
   
